0890c3256a784c5b9dce54d8188a769ad4137bf3
ke975 <60978634+ke975@users.noreply.github.com>
2021-06-26 11:17:22 -0600

    creacion de la tercera y cuarta pantalla

--- screens/Creditos.js
+++ screens/Creditos.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { ScrollView,Text} from 'react-native';
+import React from "react";
+import { StyleSheet, Image, View, Text } from "react-native";
 
 // import { Container } from './styles';
 
 const Creditos = () => {
-  return(
-<ScrollView > 
-<Text>hola muundo</Text>
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.Image}
+        source={require("../assets/Imagen_Prueba.jpg")}
+      />
 
-</ScrollView>
-  )
-}
+      <View style={styles.TextView}>
+        <Text style={styles.Text}>Centro de Innovación Farem-Estelí</Text>
+      </View>
+    </View>
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
 
-
+  Text: {
+    textAlign: "justify",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  TextView: {
+    top: 30,
+  },
+});
 
 export default Creditos;
--- screens/Instructions.js
+++ screens/Instructions.js
@@ -1,10 +1,99 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 // import { Container } from './styles';
 
 const Instructions = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleSpace}>
+        <Text style={styles.title}>Tabla de Nutrientes</Text>
+      </View>
+      <View style={styles.tableHeader}>
+        <Text style={styles.TextHeader}>Calció</Text>
+        <Text style={styles.TextHeader}>Grasas</Text>
+        <Text style={styles.TextHeader}>Calorias</Text>
+      </View>
+
+      <View style={styles.tableBody}>
+        <Text style={styles.TextBody}>100Kg</Text>
+        <Text style={styles.TextBody}>50Kg</Text>
+        <Text style={styles.TextBody}>159Kg</Text>
+      </View>
+
+      <View style={styles.tableBody}>
+        <Text style={styles.TextBody}>100Kg</Text>
+        <Text style={styles.TextBody}>50Kg</Text>
+        <Text style={styles.TextBody}>159Kg</Text>
+      </View>
+
+      <View style={styles.tableBody}>
+        <Text style={styles.TextBody}>100Kg</Text>
+        <Text style={styles.TextBody}>50Kg</Text>
+        <Text style={styles.TextBody}>159Kg</Text>
+      </View>
+
+      <View style={styles.tableBody}>
+        <Text style={styles.TextBody}>100Kg</Text>
+        <Text style={styles.TextBody}>50Kg</Text>
+        <Text style={styles.TextBody}>159Kg</Text>
+      </View>
+
+      <View style={styles.tableBody}>
+        <Text style={styles.TextBody}>100Kg</Text>
+        <Text style={styles.TextBody}>50Kg</Text>
+        <Text style={styles.TextBody}>159Kg</Text>
+      </View>
+      
+     
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  titleSpace: {
+    top: 15,
+    height: 100,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tableHeader: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderBottomColor: "black",
+    borderWidth: 1,
+    backgroundColor: "purple",
+  },
+  TextHeader: {
+    fontSize: 25,
+    padding: 5,
+    margin: 7,
+    color: "white",
+  },
+  tableBody: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderBottomColor: "black",
+    borderWidth: 1,
+    backgroundColor: "skyblue",
+  },
+  TextBody: {
+    fontSize: 25,
+    padding: 5,
+    marginRight: 27,
+    color: "black",
+  }
+});
 
 export default Instructions;
